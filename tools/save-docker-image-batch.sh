5300b43405c77ebfc8af11d267ded85382506456
lumieru <lumieru@gmail.com>
2019-07-02 12:52:56 +0800

    update to 1.8.3

--- tools/save-docker-image-batch.sh
+++ tools/save-docker-image-batch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR=$(cd "$(dirname "$0")";pwd)
-./save-docker-image.sh --image\="gitea/gitea:1.7.4" --target-dir=$DIR/../docker
+./save-docker-image.sh --image\="gitea/gitea:1.8.3" --target-dir=$DIR/../docker
