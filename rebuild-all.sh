2e3aa9953d331364f6acfb4faf39c8fff595fbd2
lumieru <lumieru@gmail.com>
2019-11-06 00:01:11 +0800

    update to 1.9.5

--- rebuild-all.sh
+++ rebuild-all.sh
@@ -31,7 +31,8 @@ declare -A versions;      declare -a orders;
 #versions["1.7"]="42"; orders+=( "1.7" )
 #versions["1.7.1"]="42"; orders+=( "1.7.1" )
 #versions["1.7.4"]="42"; orders+=( "1.7.4" )
-versions["1.8.3"]="47"; orders+=( "1.8.3" )
+#versions["1.8.3"]="47"; orders+=( "1.8.3" )
+versions["1.9.5"]="48"; orders+=( "1.9.5" )
 
 for i in "${!orders[@]}"
 do
--- tools/save-docker-image-batch.sh
+++ tools/save-docker-image-batch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR=$(cd "$(dirname "$0")";pwd)
-./save-docker-image.sh --image\="gitea/gitea:1.8.3" --target-dir=$DIR/../docker
+./save-docker-image.sh --image\="gitea/gitea:1.9.5" --target-dir=$DIR/../docker
